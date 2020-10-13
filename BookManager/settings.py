a5be090ff98b38b0bfeb71bf1b79804d8d659a4a
TeamsSix <49087564+teamssix@users.noreply.github.com>
2020-10-13 16:22:35 +0800

    Update settings.py

--- BookManager/settings.py
+++ BookManager/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ')x#rf!6p*l34$5vtp7^mckbg(m#4#v2sfo@fygt7cq$j#9r9on'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
